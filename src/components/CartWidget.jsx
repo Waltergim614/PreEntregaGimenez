254b2e86f6aeb01aa4e0b41fe282e4fd829a51f8
Walter <walygim@gmail.com>
2023-10-31 19:35:02 -0300

    preentega1

---
+++ src/components/CartWidget.jsx
@@ -0,0 +1,11 @@
+import cart from "../assets/cart.png"
+
+
+export const CartWidget = () => {
+    return (
+    <>
+    <img src={cart} alt="Carrito" width="30px" /> 
+    <span>3</span>
+    </>
+    );
+};
